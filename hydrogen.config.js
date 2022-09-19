6cd891dc8c7216e40862361de30c3f0deb7d19f5
tombeck <t@ocupop.com>
2022-09-19 11:15:11 -0700

    pushing storefront credentials

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,9 +1,9 @@
-import {defineConfig} from '@shopify/hydrogen/config';
+import { defineConfig } from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
-    storefrontApiVersion: '2022-07',
+    storeDomain: 'bittercube.myshopify.com',
+    storefrontToken: '6798b103a3a251b7e12bacebe00bd5a8',
+    storefrontApiVersion: '2022-10',
   },
 });
