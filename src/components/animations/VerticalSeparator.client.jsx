5ed46d6c4a0737668f04225429da5056a3b6c562
Ocupop Nate <nate@ocupop.com>
2022-10-18 10:45:42 -0500

    animation timing

--- src/components/animations/VerticalSeparator.client.jsx
+++ src/components/animations/VerticalSeparator.client.jsx
@@ -2,11 +2,7 @@ import { useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 
-
-
-
 export default function VerticalSeparator() {
-
 gsap.registerPlugin(ScrollTrigger);
 
 
@@ -43,7 +39,6 @@ ScrollTrigger.create({
 });
 
 
-
   return (
   <>
     <div className="w-0.5 h-full center-absolute hidden md:flex overflow-hidden" id="vertTrigger" >
@@ -59,7 +54,8 @@ ScrollTrigger.create({
         <div className="ornament-arrow ornament-arrow-right"></div>
       </div>
       <div className="absolute top-0 px-6 py-2 text-sm tracking-widest text-white uppercase -translate-x-1/2 left-1/2 h-fit bg-gold ">
-        Recipe Collection
+        {/* TODO: accept prop to update the text */}
+        Lorem Ipsum
         <div className="absolute top-0 left-0 z-10 w-full h-full bg-paper bg-reveal"></div>
       </div>
     </div>
