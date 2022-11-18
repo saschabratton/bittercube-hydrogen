41f3ab65d6c983aa4150eafe14c5b1904208d9b8
tombeck <t@ocupop.com>
2022-11-18 06:49:24 -0800

    environment var

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -3,13 +3,13 @@ import { defineConfig, CookieSessionStorage } from '@shopify/hydrogen/config'
 export default defineConfig({
   shopify: {
     // storeDomain: Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'bittercube.myshopify.com',
-    storeDomain: process?.env?.['PUBLIC_STORE_DOMAIN'] || 'bittercube.myshopify.com',
+    storeDomain: process?.env['PUBLIC_STORE_DOMAIN'] || 'bittercube.myshopify.com',
     /* Your app's public Storefront API access token. Authenticates browser and client requests. */
     // storefrontToken: Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN || '6798b103a3a251b7e12bacebe00bd5a8',
-    storefrontToken: process?.env ? ['PUBLIC_STOREFRONT_API_TOKEN'] || '6798b103a3a251b7e12bacebe00bd5a8',
+    storefrontToken: process?.env['PUBLIC_STOREFRONT_API_TOKEN'] || '6798b103a3a251b7e12bacebe00bd5a8',
     /* Your app's private Storefront API server (delegate access) token. Authenticates server requests. */
     // privateStorefrontToken: Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
-    privateStorefrontToken: process?.env?.['PRIVATE_STOREFRONT_API_TOKEN'],
+    privateStorefrontToken: process?.env['PRIVATE_STOREFRONT_API_TOKEN'],
     /* The Storefront API version that your app uses */
     storefrontApiVersion: '2022-10',
     // /* (Optional) The unique ID for the storefront. Only gets assigned by Oxygen */
---
+++ src/Error.server.jsx
@@ -0,0 +1,34 @@
+import { Seo, Link } from "@shopify/hydrogen"
+import { Layout } from '@server'
+import { HorizontalSeperator, PrimaryMenu } from "@client"
+// ----------------------------------------------------------------------
+export default function NotFound({ response }) {
+  if (response) {
+    // response.status = 404
+    // response.statusText = 'Not found'
+  }
+
+  return (
+    <Layout>
+      <section className="relative flex flex-col flex-1 h-full py-28 bg-dark bg-pattern header-dark">
+        <PrimaryMenu dark={true} />
+        <div className="container max-w-5xl text-center">
+          <div className="h-0.5 w-20 mx-auto bg-gold grow-outward mb-8"></div>
+          <h1>Oh, geez.<br />You took a few too many dashes in the wrong direction.</h1>
+          <div className="max-w-sm mx-auto mt-8">
+            <HorizontalSeperator />
+          </div>
+          <p className="mt-16 label text-ornament text-gold">
+            looks like a 404 error, but we’re not bitter about it.
+          </p>
+          <div className="mx-auto mt-8 md:w-60">
+            <Link to="/" className="w-full btn btn-action">Take me back home</Link>
+            <Link to="/" className="w-full btn btn-action">Take me shopping</Link>
+          </div>
+
+
+        </div>
+      </section>
+    </Layout>
+  )
+}
