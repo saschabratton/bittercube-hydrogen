1982fa687209a7f818626f11c9a702bd1d894fc0
tombeck <t@ocupop.com>
2022-11-18 05:48:23 -0800

    temp remove default seo for testing

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from '@shopify/hydrogen/config';
+import { defineConfig } from '@shopify/hydrogen/config'
 
 export default defineConfig({
   shopify: {
     storeDomain: 'bittercube.myshopify.com',
+    /* Your app's public Storefront API access token. Authenticates browser and client requests. */
+    // storefrontToken: process.env.PUBLIC_STOREFRONT_API_TOKEN,
+    // storefrontToken: process?.env['PUBLIC_STOREFRONT_API_TOKEN'],
     storefrontToken: '6798b103a3a251b7e12bacebe00bd5a8',
+    // /* Your app's private Storefront API server (delegate access) token. Authenticates server requests. */
+    // // privateStorefrontToken: process.env.PRIVATE_STOREFRONT_API_TOKEN,
+    /* The Storefront API version that your app uses */
     storefrontApiVersion: '2022-10',
+    // /* (Optional) The unique ID for the storefront. Only gets assigned by Oxygen */
+    // storefrontId: process?.env['PUBLIC_STOREFRONT_ID']
   },
+  /* A path to a custom page to render when the server encounters an unhandled exception */
+  // serverErrorPage: '/src/Error.server.jsx',
   logger: {
     showQueryTiming: true
   }
-});
+})
--- src/App.server.jsx
+++ src/App.server.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import renderHydrogen from '@shopify/hydrogen/entry-server';
+import renderHydrogen from '@shopify/hydrogen/entry-server'
 import {
   Router,
   FileRoutes,
   ShopifyProvider,
   CartProvider,
-} from "@shopify/hydrogen";
-import {Suspense} from 'react';
-import { DefaultSeo } from "./components/global/DefaultSeo.server";
+} from "@shopify/hydrogen"
+import { Suspense } from 'react'
+// import { DefaultSeo } from "./components/global/DefaultSeo.server";
 
 function App() {
   return (
     <Suspense fallback={null}>
       <ShopifyProvider>
         <CartProvider>
-          <Suspense>
+          {/* <Suspense>
             <DefaultSeo />
-          </Suspense>
+          </Suspense> */}
           <Router>
             <FileRoutes />
           </Router>
