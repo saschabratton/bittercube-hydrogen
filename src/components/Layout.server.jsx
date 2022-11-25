8e5c9c7379d5a9c985305148c70e11fd020f74a3
Ocupop Nate <nate@ocupop.com>
2022-11-25 10:14:59 -0600

    free shipping banner

--- src/components/Layout.server.jsx
+++ src/components/Layout.server.jsx
@@ -6,11 +6,14 @@ export default function Layout({ children }) {
 
   return (
     <div className="relative flex flex-col min-h-screen antialiased bg-neutral-50">
-      <main role="main" id="mainContent" className="flex-grow bg-paper">
+      <main role="main" id="mainContent" className="relative flex-grow bg-paper">
         <Suspense>
           {children}
         </Suspense>
       </main>
+      <div className="fixed bottom-0 left-0 z-10 flex justify-center w-full p-8 bg-dark">
+          <span className="text-gold label">BLACK FRIDAY - 25% OFF BITTERS AND KITS + FREE SHIPPING ON $45+ ORDERS</span>
+      </div>
       <Footer />
     </div>
   )
