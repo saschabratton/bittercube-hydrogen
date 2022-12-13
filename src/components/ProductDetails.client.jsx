f85452fa48d0b02d2a03ec7617ca009f473381cc
Ocupop Nate <nate@ocupop.com>
2022-12-13 10:29:53 -0600

    removed parse descrpition

--- src/components/ProductDetails.client.jsx
+++ src/components/ProductDetails.client.jsx
@@ -23,9 +23,9 @@ export default function ProductDetails({ product }) {
             <p className="text-base label">{product.vendor}</p>
             <h1>{product.title}</h1>
             <ProductForm product={product} />
-            <div className="pt-6 prose text-dark text-md">
+            {/* <div className="pt-6 prose text-dark text-md">
               {parse(product.descriptionHtml)}
-            </div>
+            </div> */}
 
 
             {/* <button href="#" className="btn btn-action" disabled>Add to cart</button> */}
