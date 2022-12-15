270e3b1142562d43a2a6dcf01a26f899de3a72b4
Ocupop Nate <nate@ocupop.com>
2022-12-15 13:53:22 -0600

    product columns

--- src/components/BittersBundles.server.jsx
+++ src/components/BittersBundles.server.jsx
@@ -17,7 +17,7 @@ export default function BittersBundles() {
 
 
   return(
-    <div className="container grid grid-cols-2 gap-6 md:grid-cols-4">
+    <div className="container grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
         {collection.products.nodes.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
